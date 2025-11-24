44b85084301a2b234763924af9e9b8b503816532
KimJiGwang <5244416@naver.com>
2025-11-25 01:30:59 +0900

    rag 기능들 추가

---
+++ alphabot-back/app/models/news_vector.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column,Text,DateTime,Integer,ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+from pgvector.sqlalchemy import Vector
+import uuid
+
+from aibackend.app.services.news_vector_service import NewsBase
+
+class NewsVector(NewsBase):
+    __tablename__="news_vectors"
+    
+    id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    title = Column(Text,nullable=False)
+    content = Column(Text,nullable=False)
+    embedding = Column(Vector(1536))
+    published_at = Column(DateTime)
+    
+    chunks = relationship("chunkVector", back_populates="news")
+
+class chunkVector(NewsBase):
+    __tablename__="news_chunks"
+    
+    id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    news_id = Column(UUID(as_uuid=True),ForeignKey("news_vectors.id"),nullable=False)
+    chunk_index = Column(Integer,nullable=False)
+    chunk_text = Column(Text,nullable=False)
+    embedding = Column(Vector(1536))
+    published_at = Column(DateTime)
+    title = Column(Text)
+    
+    news = relationship("NewsVector",back_populates="chunks")
